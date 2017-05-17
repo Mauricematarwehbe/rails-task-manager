ffa86f1eee2a038cef8c35ba05b81cf96ced92f2
Maurice Matar Wehbe <mauricematarwehbe93@gmail.com>
2017-05-17 03:25:19 -0700

    exo 1 fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # #List tasks
+  # get 'tasks', to: 'tasks#index'
+  # #List a specific task
+  # get 'task/:id', to: 'tasks#show', as:
+  # #Create a task
+  # get 'task/new', to: 'tasks#new'
+  # post'tasks', to: 'tasks#create'
+  # #Update a task
+  # get 'task/:id/edit', to: 'task#edit'
+  # patch 'restaurants/:id', to: 'task#update'
+  # #Destroy a task
+  # delete 'task/:id', to: 'task#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "Fromager", description: "Acheter du Napoleon")
+Task.create(name: "Italien", description: "Récupérer les scarmossa")
+Task.create(name: "SNCF", description: "Renouveler la carte")
